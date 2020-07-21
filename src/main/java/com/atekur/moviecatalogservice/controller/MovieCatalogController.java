fb124b60d43053f695e468db3d48e6648b17b671
Atekur Rahman <jp7atik@gmail.com>
2020-07-21 20:13:37 +0600

    Added some models, call movie info service and movie rating service

--- src/main/java/com/atekur/moviecatalogservice/controller/MovieCatalogController.java
+++ src/main/java/com/atekur/moviecatalogservice/controller/MovieCatalogController.java
@@ -1,19 +1,33 @@
 package com.atekur.moviecatalogservice.controller;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.atekur.moviecatalogservice.models.CatalogItem;
+import com.atekur.moviecatalogservice.models.Movie;
+import com.atekur.moviecatalogservice.models.UserRatings;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		return Collections.singletonList(new CatalogItem("Transformer", "Si-Fi movie", 4));
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+		UserRatings userRatings = restTemplate.getForObject("http://localhost:8083/rating/user/" + userId, UserRatings.class);
+
+		return userRatings.getUserRating().stream().map(rating -> {
+			Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+			return new CatalogItem(movie.getName(), "Si-Fi movie", rating.getRating());
+		}).collect(Collectors.toList());
 	}
 }
---
+++ src/main/java/com/atekur/moviecatalogservice/models/Movie.java
@@ -0,0 +1,34 @@
+package com.atekur.moviecatalogservice.models;
+
+public class Movie {
+	private String name;
+	private String movieId;
+	
+	public Movie() {
+		
+	}
+	
+	public Movie(String name, String movieId) {
+		super();
+		this.name = name;
+		this.movieId = movieId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	
+	
+}
